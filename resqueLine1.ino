12f4b85ac3dc23d6e57bf28bdf43f857ea2c2b82
dmitrypavlikov <56763340+dmitrypavlikov@users.noreply.github.com>
2021-05-05 16:07:01 +0300

    Line 1.4
    Boolean PID (dont work)

--- resqueLine1.ino
+++ resqueLine1.ino
@@ -1,4 +1,5 @@
 #include <QTRSensors.h>
+QTRSensors qtr;
 
 //Моторы в бане
 #define leftmotor_dir 44 
@@ -6,24 +7,20 @@
 #define rightmotor_dir 46
 #define rightmotor_spd 47
 
-QTRSensors qtr;
 const uint8_t SensorCount = 8;
 int sensorValues[SensorCount];
-int spd = 60;
+int spd = 50;
 
 
 int err = 0;
 float integral = 0;
 float lastErr = 0;
 float kp = 1;
-float ki = 1;
-float kd = 1;
-float dt = 0.05;
+float ki = 0.1;
+float kd = 0;
+float dt = 1;
 float OUT = 0;//Переменная, в которую будут поступать данные из ПИДа. Просто для удобства, можно удалить.
 
-//int lastFlag, currentFlag;
-
-//Функция движения, остаётся просто для примера ниже.
 void moving (bool Left, bool Right, int spd_L, int spd_R){
   digitalWrite(leftmotor_dir, Left);
   analogWrite(leftmotor_spd, spd_L);
@@ -49,7 +46,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);//Лампочка на плате для удобного отслеживания процесса калибровки.
   
   //Калибровка - снимаем 20 показаний с каждого датчика, находим максимум и мунимум (чёрный и белый).
-    for (uint8_t i = 0; i < 10; i++){
+   for (uint8_t i = 0; i < 10; i++){
       //moving(0,0,0,0); В дальнейшем робот должен двигаться во время калибровки.
        digitalWrite(LED_BUILTIN, HIGH);
        qtr.calibrate();
@@ -59,31 +56,23 @@ void setup() {
        delay(500);
     }
 }
-/*int PID(int input, int setpoint, float kp, float ki, float kd, float dt, int minOut, int maxOut) {
-  float err = setpoint - input;
-  float integral = 0, prevErr = 0;
-  integral = constrain(integral + (float)err * dt * ki, minOut, maxOut);
-  float D = (err - prevErr) / dt;
-  prevErr = err;
-  //return constrain(err * kp + integral + D * kd, minOut, maxOut);
-  return err * kp + integral + D * kd;
-}*/
 
 void loop() {
-  //digitalWrite(LED_BUILTIN, HIGH);
-  int local_k = -4; //Число от -4 до +4 (без нуля), на которое домножаем ошибку с каждого датчика слева направо [-4,-3,-2,-1,+1,+2,+3,+4].
+  qtr.read(sensorValues);
+  int local_k = -4;
   err = 0;
   for(uint8_t i = 0; i<8;i++){
-    //int tmp = (qtr.calibrationOn.maximum[i]+qtr.calibrationOn.minimum[i])/2;
-    /*if(sensorValues[i]<tmp){
+    int tmp = (qtr.calibrationOn.maximum[i]+qtr.calibrationOn.minimum[i])/2;
+    if(sensorValues[i]<tmp){
       sensorValues[i]=0;
     }
     else{
       sensorValues[i]=1;
-    }*/
-    //Serial.print(((qtr.calibrationOn.maximum[i]+qtr.calibrationOn.minimum[i])/2 - sensorValues[i]) * local_k);
-    //Serial.print(" ");
-    err += ((qtr.calibrationOn.maximum[i]+qtr.calibrationOn.minimum[i])/2 - sensorValues[i]) * local_k;
+    }
+    Serial.print(sensorValues[i]* local_k);
+    Serial.print(" ");
+  
+    err += sensorValues[i] * local_k;
     local_k++;
     if (local_k == 0){
       local_k=1;// Ноль пропустили
@@ -91,94 +80,29 @@ void loop() {
     //Serial.print(sensorValues[i]);
     //Serial.print(" ");
   }
-  Serial.println();
-   
-   qtr.read(sensorValues);
-   
-   //ПИД
-   err = 0 - err;
-   integral = constrain(integral + err * dt * ki, -1000, 1000);
-   float D = (err - lastErr) / dt;
-   lastErr = err;
-   OUT = constrain(err * kp + integral + D * kd, -1000, 1000);
-   moving(1,1,spd+OUT,spd-OUT);
+  //Serial.print("err = ");
+  //Serial.print(err);
 
+   //ПИД
+  err = (0 - err);
+  if (err == 0) {
+    integral = 0;
+  }
+  else {
+    integral = integral + err * dt * ki;
+  }
+  float D = (err - lastErr) / dt;
+  lastErr = err;
+  OUT = err * kp + integral + D * kd;
+  moving(1,1,spd+OUT,spd-OUT);
 
   Serial.print(err);
   Serial.print(" ");
+  Serial.print(integral);
+  Serial.print(" ");
   Serial.print(OUT);
   Serial.println();
-  
-  
-/* if(sV[i]<tmp){
-      sV[i]=0;
-    }
-    else{
-      sV[i]=1;
-    }*/
-//    Serial.print(sV[i]);
-  //  Serial.print('\t');
-  }
-/*
-if((sV[3]==1)||(sV[4]==1)){
- currentFlag = 34;
 }
-if((sV[1]==0)&&(sV[1]==0)&&(sV[2]==0)&&(sV[3]==0)&&(sV[4]==0)&&(sV[5]==0)&&(sV[6]==0)&&(sV[7]==0)){
- switch(lastFlag){
-  case 7: moving(0,1,spd,spd);
-  case 4: moving(1,1,spd,spd);
-  case 3: moving(1,1,spd,spd);
-  case 0: moving(1,0,spd,spd);
- }
-}*/
-/*if((sV[1]==1)||(sV[2]==1)){
- moving(1,1,spd/2,spd);
-}
-if((sV[5]==1)||(sV[6]==1)){
- moving(1,1,spd,spd/2);
-}
-if(sV[0]==1){
- moving(0,1,spd/5,spd+spd/5);
-}
-if(sV[7]==1){
- moving(1,0,spd+spd/5,spd/5);
-}
-if(sV[7]==1){
- moving(1,0,spd+spd/6,spd/2);
- currentFlag = 7;
-}
-if(sV[6]==1){
- moving(1,1,spd+spd/6,spd/3);
- currentFlag = 6;
-}
-if(sV[5]==1){
- moving(1,1,spd+spd/6,spd/2);
- currentFlag = 5;
-}
-if(sV[4]==1){
- moving(1,1,spd,spd);
- currentFlag = 4;
-}
-if(sV[3]==1){
- moving(1,1,spd,spd);
- currentFlag = 3;
-}
-if(sV[2]==1){
- moving(1,1,spd/2,spd+spd/6);
- currentFlag = 2;
-}
-if(sV[1]==1){
- moving(1,1,spd/3,spd+spd/6);
- currentFlag = 1;
-}
-if(sV[0]==1){
- moving(0,1,spd/4,spd+spd/6);
- currentFlag = 0;
-}*/
-// lastFlag = currentFlag;
-  //Serial.print(lastFlag); 
-  //Serial.print('-');
-  //Serial.print(currentFlag);
-  //Serial.print(' ');
-//  Serial.println(); 
-//  }
+
+
+
