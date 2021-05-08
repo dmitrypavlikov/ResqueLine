6867b958a90823bb14130840a53deba13c061ecd
dmitrypavlikov <56763340+dmitrypavlikov@users.noreply.github.com>
2021-05-09 01:35:23 +0300

    Line 1.6
    Great chance to work!

--- resqueLine1.ino
+++ resqueLine1.ino
@@ -1,8 +1,15 @@
+#include <VarSpeedServo.h>  //Установи библиотеку, если не работает. Но много времени ей можно не уделяем, пока не поедем
 #include <QTRSensors.h>
 #include <NewPing.h>
 
-NewPing sonar(6, 7, 500);
+NewPing sonar(8, -, 500); // Сонар: Trig = 8, Echo нужно указать.
+
 QTRSensors qtr;
+
+VarSpeedServo leftServo; //Объекты серв
+VarSpeedServo rightServo; //Объекты серв
+const int leftServoPin = 6; // the digital pin used for the first servo
+const int rightServoPin = 7; // the digital pin used for the second servo
 
 struct Color {
   int S0;
@@ -12,74 +19,91 @@ struct Color {
   int sensorOut;
   int red, green, blue;
 };
-Color leftColor = Color{12, 3, 4, 5, 6, 0, 0, 0};
-Color rightColor = Color{7, 8, 9, 10, 11, 0, 0, 0};
+Color leftColor = Color{-,-,-,-,4, 0, 0, 0};       //Указать Digital пины.
+Color rightColor = Color{-,-,-,-, 5, 0, 0, 0};     //Указать Digital пины.
 
 struct Go {
   int forward;
   int revers;
   int spd;
 };
-Go leftMotor = Go{9, 10, 11};
-Go rightMotor = Go{8, 7, 6};
+Go leftMotor = Go{-, -, 2};  //Указать Digital пины. ENA и EMB могут быть перепутаны.
+Go rightMotor = Go{-, -, 3}; //Указать Digital пины. ENA и EMB могут быть перепутаны.
+
+
+ 
+
+ 
+  
 
 struct {
   int err = 0;
   float integral = 0;
   float lastErr = 0;
   float kp = 3;
-  float ki = 0.5;
-  float kd = 0.5;
+  float ki = 0.4;
+  float kd = 0.3;
   float dt = 1;
-  float OUT = 0;//Переменная, в которую будут поступать данные из ПИДа. Просто для удобства, можно удалить.
-  int plank = 0;
+  float OUT = 0;  //Переменная, в которую будут поступать данные из ПИДа.
+  int plank = 0;  //Переменная для среднего числа датчика линии.
 } pid;
 
 
 
 const uint8_t SensorCount = 8;
 int sensorValues[SensorCount];
-int tmp = 0; //для того чтобы лишь 1 раз назначить выходы пинов
+int tmp = 0; 
+int sped = 20;
+int freq = 0;
 
 
-
-//функция для датчиков цвета color(leftcolor, 50
-void color(Color funcColor, int freq = 0) {
-  if (tmp = 0) {
-    pinMode(funcColor.S0, OUTPUT);
+//функция для датчиков цвета
+void color(Color funcColor) {
+    pinMode(funcColor.S0, OUTPUT); 
     pinMode(funcColor.S1, OUTPUT);
     pinMode(funcColor.S2, OUTPUT);
     pinMode(funcColor.S3, OUTPUT);
     pinMode(funcColor.sensorOut, INPUT);
     digitalWrite(funcColor.S0, HIGH);
     digitalWrite(funcColor.S1, LOW);
-    tmp++;
-  }
 
   //R
   digitalWrite(funcColor.S2, 0);
   digitalWrite(funcColor.S3, 0);
   freq = pulseIn(funcColor.sensorOut, 0);
   funcColor.red = map(constrain(freq, 70, 120), 70, 120, 255, 0);
-  delay(100);
+  Serial.print("\t");
+  Serial.print("R");
+  Serial.print(funcColor.red);
+
 
   //G
   digitalWrite(funcColor.S2, 1);
   digitalWrite(funcColor.S3, 1);
   freq = pulseIn(funcColor.sensorOut, 0);
   funcColor.green = map(constrain(freq, 100, 200), 100, 200, 255, 0);
-  delay(100);
+  Serial.print("\t");
+  Serial.print("G");
+  Serial.print(funcColor.green);
 
   //B
   digitalWrite(funcColor.S2, 0);
   digitalWrite(funcColor.S3, 1);
   freq = pulseIn(funcColor.sensorOut, 0);
   funcColor.blue = map(constrain(freq, 60, 100), 60, 100, 255, 0);
-  delay(100);
+  Serial.print("\t");
+  Serial.print("B");
+  Serial.print(funcColor.blue);
+  Serial.println();
 }
 
 void setup() {
-
+  
+  leftServo.attach(leftServoPin);  // Привязка левой сервы к пину 6
+  leftServo.write(0,255,false); // Начальное положение, выставляется с максимальной скоростью
+  rightServo.attach(rightServoPin);  // Привязка правой сервы к пину 7
+  rightServo.write(0,255,true);  // set the intial position of the servo, as fast as possible, wait until done
+  
   pinMode(LED_BUILTIN, OUTPUT);//Лампочка на плате для удобного отслеживания процесса калибровки.
 
   qtr.setTypeAnalog();
@@ -88,9 +112,9 @@ void setup() {
   }, SensorCount);
   qtr.setEmitterPin(2);
 
-  //Калибровка - снимаем 20 показаний с каждого датчика, находим максимум и мунимум (чёрный и белый).
+  //Калибровка - снимаем 20 показаний с каждого датчика 10 раз, находим максимум и мунимум (чёрный и белый).
   for (uint8_t i = 0; i < 10; i++) {
-    //moving(0,0,0,0); В дальнейшем робот должен двигаться во время калибровки.
+    //moving(); В дальнейшем робот должен двигаться во время калибровки.
     digitalWrite(LED_BUILTIN, HIGH);
     qtr.calibrate();
     delay(500);
@@ -103,7 +127,6 @@ void setup() {
 
 }
 
-//Функция движения, остаётся просто для примера ниже.
 void moving (Go funcMotor, bool forvard, bool revers, int spd) {
   if (spd <= -1) {
     digitalWrite(funcMotor.forward, revers);
@@ -117,20 +140,20 @@ void moving (Go funcMotor, bool forvard, bool revers, int spd) {
 }
 
 void loop() {
-  //Serial.println(sonar.ping_cm());
+  //Serial.println(sonar.ping_cm()); Проверка соника
   qtr.read(sensorValues);
   int local_k = -4; //Число от -4 до +4 (без нуля), на которое домножаем ошибку с каждого датчика слева направо [-4,-3,-2,-1,+1,+2,+3,+4].
   pid.err = 0;
   for (uint8_t i = 0; i < 8; i++) {
-    pid.plank = (qtr.calibrationOn.maximum[i] + qtr.calibrationOn.minimum[i]) / 2;
-    if (sensorValues[i] < pid.plank) {
-      sensorValues[i] = 0;
-    }
+    pid.plank = (qtr.calibrationOn.maximum[i] + qtr.calibrationOn.minimum[i]) / 2;  //ВОТ ТУТ ЕСТЬ ИДЕЯ, КОТОРУЮ НУЖНО ПРОВЕРИТЬ:
+    if (sensorValues[i] < pid.plank) {                  //ЧТО, ЕСЛИ СОРТИРОВАТЬ ЛИНИЮ НЕ ПО ЦЕНТРАЛЬНОМУ ЗНАЧЕНИЮ, А ПО ЗНАЧЕНИЮ НЕМНОГО БОЛЬШЕ (1.5)
+      sensorValues[i] = 0;                              //ПРИМЕР: ДИАПАЗОН 0-100. СРЕДНЕЕ ЧИСЛО = 50; 50 * 1.5 = 75. В ИТОГЕ ЛИНИЯ ЭТО ТО, ЧТО БОЛЬШЕ ЧЕМ 75, А НЕ 50. //pid.plank*1.5 
+    }                                                   //ЭТО МОЖЕТ ПОДНЯТЬ СЕНСУ ДАТЧИКА ДАЖЕ НА ТАКОМ РАССТОЯНИИ (теоретически)
     else {
       sensorValues[i] = 1;
     }
-    Serial.print(sensorValues[i]* local_k);
-    Serial.print(" ");
+    //Serial.print(sensorValues[i]* local_k);
+    //Serial.print(" ");
 
     pid.err += sensorValues[i] * local_k;
     local_k++;
@@ -140,31 +163,46 @@ void loop() {
     //Serial.print(sensorValues[i]);
     //Serial.print(" ");
   }
-  Serial.print("\t");
+  //Serial.print("\t");
 
   //ПИД
   pid.err = 0 - pid.err;
-  if (pid.err == 0) {
-    pid.integral = 0;
-  }
-  else {
+  if (pid.err == 0) { // ПРЕДЛАГАЮ ИЗМЕНИТЬ УСЛОВИЕ ОБНУЛЕНИЯ I части: if ((pid.err == 0) && ((sensorValues[3] == 1) || (sensorValues[4] == 1))&& (sensorValues[ЛЮБОЕ КРОМЕ ЦЕНТРАЛЬНЫХ] == 0))
+    pid.integral = 0; // Если ошибка равна нулю, при этом центральные датчики линию видят, а крайний не видят, только тогда обнуляем I.
+  }                   // ТАКОЕ УСЛОВИЕ МОЖЕТ ЗАСТРАХОВАТЬ НАС ОТ ОБНУЛЕНИЯ I ЧАСТИ КАК НА ПОЛНОСТЬЮ БЕЛОМ, ТАК И НА ПОЛНОСТЬЮ ЧЕРНОМ, НО!!!
+  else {              // ПОСКОЛЬКУ ДАТЧИК НАХОДИТСЯ ДАЛЕКО, СЕНСА УМЕНЬШИЛАСЬ. ЕСЛИ СПОСОБ ИЗ КАЛИБРОВКИ (ЧИТАЛ ВЫШЕ?) НЕ ПОМОГ, ДУМАЕМ ДАЛЬШЕ
     pid.integral = pid.integral + pid.err * pid.dt * pid.ki;
   }
   float D = (pid.err - pid.lastErr) / pid.dt;
   pid.lastErr = pid.err;
-  //pid.OUT = map(constrain(pid.err * pid.kp + pid.integral + pid.D * pid.kd, -100000, 100000), -100000,100000, -100,100);
+  //pid.OUT = map(constrain(pid.err * pid.kp + pid.integral + pid.D * pid.kd, -100000, 100000), -100000,100000, -100,100); ТЕРЯЕТ СМЫСЛ, ТАК КАК OUT ПРИ КОЭФФИЦИЕНТЕ 3 НЕ МОЖЕТ БЫТЬ БОЛЬШЕ 27
   pid.OUT = pid.err * pid.kp + pid.integral + D * pid.kd;
-  //moving(1,1,spd-pid.OUT,1,1,spd+pid.OUT);
-
-  Serial.print(pid.err);
+  //Конец ПИДа
+  
+  
+  moving(leftMotor,1,0,spd-pid.OUT); //Движение, я мог неправильно оформить
+  moving(leftMotor,1,0,spd+pid.OUT); //Движение, я мог неправильно оформить
+  /*Serial.print(pid.err);
   Serial.print("\t");
   Serial.print(pid.OUT);
   Serial.print("\t");
-  Serial.print(" ");
+  Serial.print("  скорость -   ");
+  Serial.print(sped-pid.OUT);
   Serial.print("   ");
-  Serial.print(" ");
-  Serial.println();
+  Serial.print(sped+pid.OUT);
+  Serial.println();*/
+  color(rightColor);
 
-
-
+  
+  //leftServo.write(180,127,false);  //(градус (0-180), 127 - быстрая скорость, false значит, что серва будет будет крутиться одновременно со следующими до ближайшего true включительно)
+  //rightServo.write(180,127,true);  // тут у сервы true - это значит, что эта серва и все сервы до неё (у которых false) работают одновременно                                       
+  //leftServo.write(0,30,false);     //(градус (0-180), 30 - медленная скорость, false значит, что серва будет будет крутиться одновременно со следующими до ближайшего true включительно)     
+  //rightServo.write(0,30,true);     // если непонятно, про true и false Ваня может нормально объяснить
+  
+  //Если не получилось поднять сенсу у калибровки методом plank*1.5, из-за чего не получилось внедрить новое условие для обнуления I части ПИДа, или просто не сработало, 
+  //Тут могут быть switch case по последнему положению lastFlag. Ссылка на код, где они остались  https://github.com/dmitrypavlikov/ResqueLine/blob/line-1.0/resqueLine1.ino 
+  
+  
+    
+Serial.println();
 }
