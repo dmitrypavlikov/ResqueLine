041a4e5fb3b74ae9d02cff6cfb848dfaac3f8568
dmitrypavlikov <56763340+dmitrypavlikov@users.noreply.github.com>
2021-05-10 21:38:21 +0300

    Line 1.6.2
    map vs alpha-beta-gamma

--- resqueLine1.ino
+++ resqueLine1.ino
@@ -2,14 +2,14 @@
 #include <QTRSensors.h>
 #include <NewPing.h>
 
-//NewPing sonar(8, -, 500); // Сонар: Trig = 8, Echo нужно указать.
+NewPing sonar(8, 7, 500); // Сонар: Trig = 8, Echo нужно указать.
 
 QTRSensors qtr;
 
 /*VarSpeedServo leftServo; //Объекты серв
   VarSpeedServo rightServo; //Объекты серв
-  const int leftServoPin = 6; // the digital pin used for the first servo
-  const int rightServoPin = 7; // the digital pin used for the second servo
+  const int leftServoPin = 9; // the digital pin used for the first servo
+  const int rightServoPin = 10; // the digital pin used for the second servo
 */
 struct Color {
   int S0;
@@ -19,9 +19,8 @@ struct Color {
   int sensorOut;
   int red, green, blue;
 };
-//Color leftColor = Color{-,-,-,-,4, 0, 0, 0};       //Указать Digital пины.
-//Color rightColor = Color{-,-,-,-, 5, 0, 0, 0};     //Указать Digital пины.
-
+Color leftColor = Color{28,29,32,33,4, 0, 0, 0};   
+Color rightColor = Color{30,31,35,35, 5, 0, 0, 0};
 struct Go {
   int forward;
   int revers;
@@ -40,9 +39,9 @@ struct {
   int err = 0;
   float integral = 0;
   float lastErr = 0;
-  float kp = 3;
-  float ki = 0.4;
-  float kd = 0.3;
+  float kp = 10;
+  float ki = 2;
+  float kd = 2;
   float dt = 1;
   float OUT = 0;  //Переменная, в которую будут поступать данные из ПИДа.
   float plank = 0;  //Переменная для среднего числа датчика линии.
@@ -53,7 +52,7 @@ struct {
 const uint8_t SensorCount = 8;
 int sensorValues[SensorCount];
 int tmp = 0;
-int sped = 20;
+int sped = 40;
 int freq = 0;
 
 
@@ -94,7 +93,7 @@ void color(Color funcColor) {
   Serial.print("\t");
   Serial.print("B");
   Serial.print(funcColor.blue);
-  Serial.println();
+  //Serial.println();
 }
 
 
@@ -102,7 +101,7 @@ void moving (Go funcMotor, bool forvard, bool revers, int spd) {
   if (spd <= -1) {
     digitalWrite(funcMotor.forward, revers);
     digitalWrite(funcMotor.revers, forvard);
-    analogWrite(funcMotor.spd, abs(spd));
+    analogWrite(funcMotor.spd, 0);
   } else {
     digitalWrite(funcMotor.forward, forvard);
     digitalWrite(funcMotor.revers, revers);
@@ -122,12 +121,12 @@ void setup() {
 
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]) {
-    A0, A1, A2, A3, A4, A5, A6, A7
+    A7, A6, A5, A4, A3, A2, A1, A0
   }, SensorCount);
   qtr.setEmitterPin(2);
 
   //Калибровка - снимаем 20 показаний с каждого датчика 10 раз, находим максимум и мунимум (чёрный и белый).
-  for (uint8_t i = 0; i < 10; i++) {
+ for (uint8_t i = 0; i < 10; i++) {
     //moving(); В дальнейшем робот должен двигаться во время калибровки.
     digitalWrite(LED_BUILTIN, HIGH);
     qtr.calibrate();
@@ -135,23 +134,23 @@ void setup() {
     digitalWrite(LED_BUILTIN, LOW);
     qtr.calibrate();
     delay(500);
-  }
+  }/*
   //Калибровка автоматичческая
-  /*
-    for (uint8_t i = 0; i < 10; i++) {
+  delay(1000);
+    for (uint8_t i = 0; i < 15; i++) {
       moving(leftMotor,1,0,60);
       moving(rightMotor,1,0,-60);
       qtr.calibrate();}
-    for (uint8_t i = 0; i < 20; i++) {
+    for (uint8_t i = 0; i < 30; i++) {
       moving(leftMotor,1,0,-60);
       moving(rightMotor,1,0,60);
       qtr.calibrate();}
-    for (uint8_t i = 0; i < 10; i++) {
+    for (uint8_t i = 0; i < 15; i++) {
       moving(leftMotor,1,0,60);
       moving(rightMotor,1,0,-60);
        qtr.calibrate();}
-
-  */
+*/
+  
 
   Serial.begin(9600);
 
@@ -160,7 +159,7 @@ void setup() {
 
 
 void loop() {
-  //Serial.println(sonar.ping_cm()); Проверка соника
+  //Serial.println(sonar.ping_cm());
   qtr.read(sensorValues);
 
   int local_k = -4; //Число от -4 до +4 (без нуля), на которое домножаем ошибку с каждого датчика слева направо [-4,-3,-2,-1,+1,+2,+3,+4].
@@ -168,19 +167,15 @@ void loop() {
   for (uint8_t i = 0; i < 8; i++) {
     //Serial.print(sensorValues[i]);
     //Serial.print(" ");
-
-    pid.plank = ((qtr.calibrationOn.maximum[i] + qtr.calibrationOn.minimum[i]) / 1.92);
-    if (sensorValues[i] < pid.plank) {
-
-      //pid.plank = map(sensorValues[i], qtr.calibrationOn.minimum[i], qtr.calibrationOn.maximum[i], 0, 100);
-      //if (pid.plank<79){
-
+ pid.plank = map(sensorValues[i], qtr.calibrationOn.minimum[i], qtr.calibrationOn.maximum[i], 0, 100);
+      if (pid.plank<79){ // 79 - число, которое зависит исключительно от высоты, и от сенсы, которая нам нужна. Это 80% между 0-100%
       sensorValues[i] = 0;
-    }
-    sensorValues[i] = 1;
-  }
-  // Serial.print(sensorValues[i]* local_k);
-  //Serial.print(" ");
+    } else{
+    sensorValues[i] = 1;}
+  
+ Serial.print(sensorValues[i]* local_k);
+ Serial.print(" ");
+  
   //Serial.print(pid.plank);
   //Serial.print(" ");
 
@@ -196,23 +191,12 @@ void loop() {
 
 //ПИД
 pid.err = 0 - pid.err;
-if ((pid.err == 0) && (sensorValues[3] == 1)) {
+if ((pid.err == 0) && (sensorValues[3] == 1)&&(sensorValues[0] == 0)) {
   pid.integral = 0;
 }
 else {
   pid.integral = pid.integral + pid.err * pid.dt * pid.ki;
 }
-
-//ПИД№2
-/*pid.err = 0 - pid.err;
-  if ((pid.err == 0)&&(sensorValues[3] == 1)) { // &&(sensorValues[0] != 0))
-  pid.dt = 0;
-  pid.integral = 0;}
-  else{
-  pit.dt += 0.5;}
-  pid.integral = pid.integral + pid.err * pid.dt * pid.ki;
-*/
-
 float D = (pid.err - pid.lastErr) / pid.dt;
 pid.lastErr = pid.err;
 pid.OUT = pid.err * pid.kp + pid.integral + D * pid.kd;
@@ -222,8 +206,9 @@ pid.OUT = pid.err * pid.kp + pid.integral + D * pid.kd;
 
 
 
-moving(leftMotor, 1, 0, sped - pid.OUT);
-moving(rightMotor, 1, 0, sped + pid.OUT);
+//moving(leftMotor, 1, 0, sped - pid.OUT);
+//moving(rightMotor, 1, 0, sped + pid.OUT);
+
 Serial.print(pid.err);
 Serial.print("\t");
 Serial.print(pid.OUT);
@@ -232,16 +217,15 @@ Serial.print("  скорость -   ");
 Serial.print(sped - pid.OUT);
 Serial.print("   ");
 Serial.print(sped + pid.OUT);
-Serial.println();
+//color(leftColor);
+//Serial.print("\t");
 //color(rightColor);
-
 
 //leftServo.write(180,127,false);  //(градус (0-180), 127 - быстрая скорость, false значит, что серва будет будет крутиться одновременно со следующими до ближайшего true включительно)
 //rightServo.write(180,127,true);  // тут у сервы true - это значит, что эта серва и все сервы до неё (у которых false) работают одновременно
 //leftServo.write(0,30,false);     //(градус (0-180), 30 - медленная скорость, false значит, что серва будет будет крутиться одновременно со следующими до ближайшего true включительно)
 //rightServo.write(0,30,true);     // если непонятно, про true и false Ваня может нормально объяснить
 
-//Если не получилось поднять сенсу у калибровки методом plank*1.5, из-за чего не получилось внедрить новое условие для обнуления I части ПИДа, или просто не сработало,
 //Тут могут быть switch case по последнему положению lastFlag. Ссылка на код, где они остались  https://github.com/dmitrypavlikov/ResqueLine/blob/line-1.0/resqueLine1.ino
 
 
